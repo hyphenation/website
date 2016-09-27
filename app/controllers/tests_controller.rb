1c2dbdad366f41f6f93192e27a7d99411fca30f5
Arthur Reutenauer <arthur.reutenauer@normalesup.org>
2016-09-28 00:27:23 +0100

    Simplified controller variables.

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -2,15 +2,14 @@ class TestsController < ApplicationController
   def show
     @word = ''
     @languages = Language.all.sort
-    @language = nil
+    @bcp47 = nil
   end
 
   def submit
     @word = params['word']
     @bcp47 = params['bcp47']
     @languages = Language.all.sort
-    @language = Language.find_by_bcp47(@bcp47)
-    @hyphenated = @language.hyphenate(@word)
-    @params = params
+    language = Language.find_by_bcp47(@bcp47)
+    @hyphenated = language.hyphenate(@word)
   end
 end

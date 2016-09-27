759eafee33e14b8ae1a8668b7f18091c5bd8dcb7
Arthur Reutenauer <arthur.reutenauer@normalesup.org>
2016-09-28 00:10:01 +0100

    Slightly better style.

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,13 +1,16 @@
 class TestsController < ApplicationController
   def show
     @word = ''
+    @languages = Language.all.sort
+    @language = nil
   end
 
   def submit
     @word = params['word']
     @bcp47 = params['bcp47']
-    language = Language.find_by_bcp47(@bcp47)
-    @hyphenated = language.hyphenate(@word)
+    @languages = Language.all.sort
+    @language = Language.find_by_bcp47(@bcp47)
+    @hyphenated = @language.hyphenate(@word)
     @params = params
   end
 end

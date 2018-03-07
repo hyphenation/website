b19e145952ff7898b648b307ca1084d4f432c341
Arthur Reutenauer <arthur@reutenauer.eu>
2018-03-07 19:43:01 +0100

    Preparing to use rackup.

--- lib/hyphwebsite/serve.rb
+++ lib/hyphwebsite/serve.rb
@@ -1,9 +1,31 @@
 require 'sinatra'
-require File.expand_path('../../../lib/hyphwebsite', __FILE__)
+require 'tilt/haml'
+require File.expand_path('../lib/hyphwebsite/serve', __FILE__)
 
-class Application < Sinatra::Application
-  def self.call(ignore)
-  end
+get '/' do
+  haml :index
+end
 
-  private
+get '/test-patterns' do
+  fetch_languages
+  @word = ''
+  @bcp47 = nil
+  haml :show
+end
+
+post '/test-patterns' do
+  fetch_languages
+  @word = params['word']
+  @bcp47 = params['bcp47']
+  language = Language.find_by_bcp47(@bcp47)
+  @hyphenated = language.hyphenate(@word)
+  haml :submit
+end
+
+def fetch_languages
+  @languages = Language.all.sort { |a, b| a.bcp47 <=> b.bcp47 }
+end
+
+get '/relicensing' do
+  haml :relicensing
 end

f8518586ef6a2a4811498f79d1da9f8d033e104e
Arthur Reutenauer <arthur@reutenauer.eu>
2018-03-07 19:49:59 +0100

    Little bit of cleanup.

--- config.ru
+++
@@ -1,4 +0,0 @@
-# This file is used by Rack-based servers to start the application.
-
-require File.expand_path('../lib/hyphwebsite/serve',  __FILE__)
-run Application
--- lib/hyphwebsite/serve.rb
+++
@@ -1,9 +0,0 @@
-require 'sinatra'
-require File.expand_path('../../../lib/hyphwebsite', __FILE__)
-
-class Application < Sinatra::Application
-  def self.call(ignore)
-  end
-
-  private
-end
